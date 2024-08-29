75d30b12a7c15a5f128d93a689c60d45bfc742d7
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2024-08-29 15:20:30 +0100

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,117 +1,92 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import { lazy } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-[350vh]">
+      <div className="relative bg-black h-[50vh] w-full mt-12">
+        <div className="bg-pictures relative"></div>
+        <div className="text">
+          <i className="text-stroke">Capturing the essence of life</i>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="mt-12">
+        <div>Perfect len's introduction</div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div>
+          <h1 className="text-center text-orange-400 text-4xl mt-10 font-medium courgette-regular ">
+            Catalog
+          </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="pic-layout mt-6 relative w-full">
+            <img src={"/lens1.jpg"} className="pic1" />
+            <div>
+              <img src={"/lens2.jpg"} className="pic2" />
+            </div>
+            <div>
+              <img src="/lens3.jpg" className="pic3" />
+            </div>
+            <div>
+              <img src="/lens5.jpg" className="pic4" />
+            </div>
+            <div>
+              <img src="/lens6.jpg" className="pic5" />
+            </div>
+            <div>
+              <img src="/lens7.jpg" className="pic6" />
+            </div>
+            <div>
+              <img src="/lens8.jpg" className="pic7" />
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        <div className="mt-32 text-center courgette-regular text-2xl">
+          <h1>Send a message</h1>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <form className="border-orange-500 border rounded mt-6 ml-1">
+            <div className="mb-2 mt-2">
+              <label htmlFor="name" className="text-orange-500">
+                Name:{" "}
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-[300px] rounded-md text-white font-sans text-xl bg-transparent backdrop-blur-md border-orange-400 border pl-2"
+              />
+            </div>
+
+            <div className="mb-8">
+              <label htmlFor="email" className="text-orange-500 ">
+                Email:{" "}
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="w-[300px] ml-1 rounded-md text-white font-sans text-xl bg-transparent backdrop-blur-md border-orange-400 border pl-2"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="message" className="mr-[270px] text-orange-500">
+                Message:{" "}
+              </label>{" "}
+              <br />
+              <textarea
+                rows={5}
+                className="text-white rounded bg-transparent backdrop-blur-md border-orange-400 border"
+              ></textarea>
+            </div>
+
+            <button className="bg-orange-500 pl-3 pr-3 mb-3 mt-3 rounded font-medium">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
